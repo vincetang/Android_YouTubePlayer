a67033d7acc604480f247b06d28eb4f747e0a753
Vincent Tang <Vince@Vincents-MacBook-Pro.local>
2016-02-13 13:11:22 -0500

    Implementation of Youtube player, added files for StandalonePlayer

---
+++ app/src/main/java/com/vincetang/youtubeplayer/StandaloneActivity.java
@@ -0,0 +1,50 @@
+package com.vincetang.youtubeplayer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class StandaloneActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String GOOGLE_API_KEY = "AIzaSyBzCopjw9mlSxJjSMApJY1y4UxDiZ8d2ac";
+    private String YOUTUBE_VIDEO_ID = "TI455Z37o30";
+    private String YOUTUBE_PLAYLIST = "";
+    private Button btnPlayVideo;
+    private Button btnPlayPlaylist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standalone);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnPlayVideo = (Button) findViewById(R.id.btnPlayVideo);
+        btnPlayPlaylist = (Button) findViewById(R.id.btnPlaylist);
+
+        btnPlayVideo.setOnClickListener(this);
+        btnPlayPlaylist.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch(v.getId()) {
+            case R.id.btnPlayVideo:
+                break;
+            case R.id.btnPlaylist:
+                break;
+            default:
+        }
+    }
+}
